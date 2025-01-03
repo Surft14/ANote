25c3c8e0d2cb37c827bbc2919da856a3d68ec4f6
Surft14 <mr.darck31@gmail.com>
2025-01-03 15:57:30 +0300

    Добавил новые иконки, создал NoteActivity и сделал для main активите меню toolbar

---
+++ app/src/main/java/com/example/anotes/NoteActivity.kt
@@ -0,0 +1,23 @@
+package com.example.anotes
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.anotes.databinding.ActivityNoteBinding
+
+class NoteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNoteBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
