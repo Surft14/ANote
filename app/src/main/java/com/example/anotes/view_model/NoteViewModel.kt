e50d0d83060fdce681bfb7590189e7e3c9f9de02
Surft14 <mr.darck31@gmail.com>
2025-01-05 23:52:04 +0300

    Добавил логи в класс NoteViewModel

--- app/src/main/java/com/example/anotes/view_model/NoteViewModel.kt
+++ app/src/main/java/com/example/anotes/view_model/NoteViewModel.kt
@@ -1,36 +1,53 @@
 package com.example.anotes.view_model
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.anotes.db_notes.Note
 import com.example.anotes.model.NoteRepository
+import com.example.anotes.model.OperationResult
 import kotlinx.coroutines.launch
 
 
 class NoteViewModel(private val repository: NoteRepository):ViewModel() {
 
     // Получение списка заметок через LiveData
-    val allNotes: LiveData<List<Note>> = repository.getAllNotes()
+
+    fun getAllNotes(): LiveData<List<Note>>{
+        return repository.getAllNotes()
+    }
 
     // Функция для добавления заметки
     fun insertNote(note: Note) {
         viewModelScope.launch {
-            repository.insertNote(note)
+            val result = repository.insertNote(note)
+            when (result){
+                is OperationResult.Success -> Log.d("MyLog", "insertNote success")
+                is OperationResult.Error -> Log.d("MyLog", "insertNote error: ${result.exception.message}}")
+            }
         }
     }
 
     // Функция для удаления заметки
     fun deleteNote(note: Note) {
         viewModelScope.launch {
-            repository.deleteNote(note)
+            val result = repository.deleteNote(note)
+            when (result){
+                is OperationResult.Success -> Log.d("MyLog", "deleteNote success")
+                is OperationResult.Error -> Log.d("MyLog", "deleteNote error: ${result.exception.message}}")
+            }
         }
     }
 
     // Функция для обновления заметки
     fun updateNote(note: Note) {
         viewModelScope.launch {
-            repository.updateNote(note)
+            val result = repository.updateNote(note)
+            when (result){
+                is OperationResult.Success -> Log.d("MyLog", "updateNote success")
+                is OperationResult.Error -> Log.d("MyLog", "updateNote error: ${result.exception.message}}")
+            }
         }
     }
 
